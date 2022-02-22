1279052eb0938b24ae5aef7eac8cc8d8e8cb86a6
TheDarkestNightRises <91905169+TheDarkestNightRises@users.noreply.github.com>
2022-02-23 00:27:20 +0100

    Added Scene Manager

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,7 +1,9 @@
+using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
-{
+{   [SerializeField] float levelLoadDelay= 1f;
     private void OnCollisionEnter(Collision collision)
     {
         switch(collision.gameObject.tag)
@@ -11,10 +13,38 @@ public class CollisionHandler : MonoBehaviour
                 break;
             case "Finish":
                 Debug.Log("Congrats!");
-                break ;
+                StartFinishSequence();
+                break;
             default:
                 Debug.Log("Sorry you blew up");
+                StartCrashSequence();
                 break;
         }
+    }
+
+    private void StartFinishSequence()
+    {
+        GetComponent<Movement>().enabled = false;
+        Invoke("LoadNextLevel", levelLoadDelay);
+    }
+
+    private void StartCrashSequence()
+    {
+        GetComponent<Movement>().enabled = false;
+        Invoke("ReloadLevel", levelLoadDelay);
+    }
+
+    private void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (SceneManager.sceneCountInBuildSettings == nextSceneIndex) nextSceneIndex = 0;
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    private void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
     }
 }
